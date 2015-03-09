9afe6634cb8c0b6e6d5e6dcea9744679dc00e801
Mort Yao <mort.yao@gmail.com>
2015-03-09 19:27:22 +0800

    emacs.d/init-elscreen.el: [M-s h] Toggle window split

--- .emacs.d/init-elscreen.el
+++ .emacs.d/init-elscreen.el
@@ -23,6 +23,34 @@
     (setq temp-list (cdr temp-list))))
 (global-set-key (kbd "M-s SPC") 'all-buffers-elscreen)
 
+;; [M-s h] Toggle window split
+;; Vertical split shows more of each line, horizontal split shows more lines
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+	     (next-win-buffer (window-buffer (next-window)))
+	     (this-win-edges (window-edges (selected-window)))
+	     (next-win-edges (window-edges (next-window)))
+	     (this-win-2nd (not (and (<= (car this-win-edges)
+					 (car next-win-edges))
+				     (<= (cadr this-win-edges)
+					 (cadr next-win-edges)))))
+	     (splitter
+	      (if (= (car this-win-edges)
+		     (car (window-edges (next-window))))
+		  'split-window-horizontally
+		'split-window-vertically)))
+	(delete-other-windows)
+	(let ((first-win (selected-window)))
+	  (funcall splitter)
+	  (if this-win-2nd (other-window 1))
+	  (set-window-buffer (selected-window) this-win-buffer)
+	  (set-window-buffer (next-window) next-win-buffer)
+	  (select-window first-win)
+	  (if this-win-2nd (other-window 1))))))
+(global-set-key (kbd "M-s h") 'toggle-window-split)
+
 ;; Location for saving elscreen tabs / desktop sessions
 (setq pwd (concat (getenv "PWD") "/"))
 (setq desktop-dirname pwd
