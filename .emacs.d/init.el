d14179a0e6a6d73b3b43007f0b0676f913f94007
Mort Yao <mort.yao@gmail.com>
2015-07-13 10:35:02 +0800

    emacs: update web-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -237,6 +237,10 @@
 (add-to-list 'auto-mode-alist '("\.erb$" . web-mode))
 (add-to-list 'auto-mode-alist '("\.mustache$" . web-mode))
 (add-to-list 'auto-mode-alist '("\.djhtml$" . web-mode))
+(add-to-list 'auto-mode-alist '("\.html5$" . web-mode))
+(setq web-mode-code-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-markup-indent-offset 2)
 
 ;; csharp-mode: extra file associations
 ;;   *.asmx: ASP.NET Web Services source file
