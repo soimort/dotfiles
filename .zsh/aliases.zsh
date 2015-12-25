cc6218acd214628c9fb9d806d57a5111e0824035
Mort Yao <soi@mort.ninja>
2015-12-25 14:23:21 +0100

    [zsh] update aliases.zsh

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,13 +1,17 @@
 #!/usr/bin/env zsh
-#
 
-# zsh
+# Zsh
 alias zshconfig="$EDITOR $HOME/.zshrc"
 alias .,=". $HOME/.zshrc"
+alias plz='sudo $(fc -ln -1)'
 
 # coreutils
 alias cp="cp -i"
 alias mv="mv -i"
+
+# development
+alias py="python3"
+alias py2="python2"
 
 # editors
 alias v="vim"
@@ -17,10 +21,6 @@ alias e="emacs -nw"
 alias em="setsid emacs"
 alias sue="sudo emacs -u $USER"
 alias sc="setsid scite"
-
-# development
-alias py="python3"
-alias py2="python2"
 
 # git
 alias add="git add"
@@ -43,7 +43,19 @@ alias k="setsid gitk"
 # networking
 alias i="ping -c 3 www.google.com"
 alias myip="dig +short myip.opendns.com @resolver1.opendns.com"
-alias mygeoip='geoiplookup $(myip)' # use SINGLE quotation marks
+alias mygeoip='geoiplookup $(myip)'
 
-# desktop
+# Arch
+alias yao='yaourt'
+
+# GNOME
 alias sysmon="setsid gnome-system-monitor"
+
+### personal
+
+alias rs='resave'
+alias trs='trans'
+alias yg='you-get'
+
+hash -d trans=~prj/translate-shell
+hash -d you-get=~prj/you-get
