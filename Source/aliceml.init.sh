b2f7c2725cff107611a13a7910c3e017963f9495
Mort Yao <soi@mort.ninja>
2016-03-31 13:05:50 +0200

    Source/: update *.init.sh

--- Source/aliceml.init.sh
+++ Source/aliceml.init.sh
@@ -1,2 +1,2 @@
-PATH=/home/soimort/Source/aliceml/distro/bin:$PATH
-ALICE_HOME=/home/soimort/Source/aliceml/distro/share/alice
+PATH="$HOME/Source/aliceml/distro/bin:$PATH"
+ALICE_HOME="$HOME/Source/aliceml/distro/share/alice"
