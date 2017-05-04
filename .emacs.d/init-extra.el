d673e6955d3be39cf00d8848bb5301a9530088f9
Mort Yao <soi@mort.ninja>
2017-05-04 10:51:14 +0200

    .emacs.d: fix asm-mode; use nasm-mode by default

--- .emacs.d/init-extra.el
+++ .emacs.d/init-extra.el
@@ -1,4 +1,15 @@
 
+;; asm-mode
+(defun my-asm-mode-hook ()
+  ;; you can use `comment-dwim' (M-;) for this kind of behaviour anyway
+  (local-unset-key (vector asm-comment-char))
+  ;; asm-mode sets it locally to nil, to "stay closer to the old TAB behaviour"
+  (setq tab-always-indent (default-value 'tab-always-indent)))
+(add-hook 'asm-mode-hook #'my-asm-mode-hook)
+
+;; nasm-mode
+(add-to-list 'auto-mode-alist '("\\.asm?\\'" . nasm-mode))
+
 ;; text-mode
 (add-hook 'text-mode-hook 'flyspell-mode)
 
