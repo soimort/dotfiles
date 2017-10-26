2fe5838985687624459eeb1ca7bc1a5e8942dee3
Mort Yao <soi@mort.ninja>
2017-10-26 21:40:23 +0200

    .zsh/my-functions: 48go & 48get

---
+++ .zsh/my-functions.zsh
@@ -0,0 +1,31 @@
+#!/usr/bin/env zsh
+
+48go() {
+    if [[ -z "$ARCHIVE_PATH" ]]; then
+        echo 'Please set $ARCHIVE_PATH first.'
+       return 1
+    fi
+
+    if [[ $1 =~ "http" ]]; then
+        echo 'Use "48get" instead. (Are you drunk?)'
+        return 1
+    fi
+
+    if [[ -z "$1" ]]; then
+        cd $ARCHIVE_PATH && ls "$_"
+    else
+        mkdir -p $ARCHIVE_PATH/$1 && cd "$_"
+    fi
+}
+
+48get() {
+    for url in "$@"; do
+        if [[ $url =~ "7gogo" ]]; then
+            Get-755 $url
+        elif [[ $url =~ "instagram" ]]; then
+            Get-Insta $url
+        elif [[ $url =~ "twitter" ]]; then
+            Get-Tweet $url
+        fi
+    done
+}
