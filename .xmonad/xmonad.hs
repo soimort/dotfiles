a84e9793d80e047b53a5870a7de8da8551f03199
Mort Yao <soi@mort.ninja>
2019-08-19 17:52:58 +0200

    .xmonad/xmonad.hs: work with dzheader

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,15 +1,23 @@
 import XMonad
+import XMonad.Layout.Spacing
 import XMonad.Util.EZConfig
+import XMonad.Hooks.DynamicLog
 
-main = xmonad $ def
-    { terminal    = "sakura"
-    --, focusFollowsMouse
-    , borderWidth = 8
-    , modMask     = mod4Mask
-    --, workspaces
-    , normalBorderColor = "#222222"
+main = xmonad =<< statusBar "dzheader" dzenPP toggleStrutsKey myConfig
+
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+
+myConfig = def
+    { terminal = "sakura"
+    , focusFollowsMouse = False
+    , clickJustFocuses = False
+    , borderWidth = 1
+    , modMask = mod4Mask
+    , normalBorderColor = "#dddddd"
     , focusedBorderColor = "#222222"
-    --, keys = myKeys
+    , layoutHook = spacingRaw False (Border 18 12 12 12) True (Border 6 6 6 6) True $ layoutHook def
+    --, layoutHook = layoutHook def  -- uncomment this and comment above line to refresh layout
+    --, startupHook
     }
     `additionalKeys`
     [ ((mod4Mask .|. shiftMask, xK_l), spawn "xscreensaver-command -lock")
