8a55c1699cf25cf1333b198c30b2682bdce777fc
Mort Yao <soi@mort.ninja>
2019-08-28 12:49:33 +0200

    .xmonad: some useful keybindings for WS

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,7 @@
 import XMonad
 import XMonad.Layout.Spacing
 import XMonad.Util.EZConfig
+import XMonad.Actions.CycleWS
 
 main = xmonad myConfig
 
@@ -17,10 +18,20 @@ myConfig = def
     , startupHook = spawn "dzheader" >> spawn "dzquotes"
     }
     `additionalKeys`
-    [ ((mod4Mask .|. shiftMask, xK_l), spawn "xscreensaver-command -lock")
-    , ((mod4Mask .|. shiftMask, xK_p), spawn "scrot -e 'mv $f ~/Pictures/'")
-    , ((0, 0xFF61), spawn "scrot -e 'mv $f ~/Pictures/'")
-    , ((0, 0x1008FF11), spawn "amixer -q sset Master 2%-")
-    , ((0, 0x1008FF13), spawn "amixer -q sset Master 2%+")
-    , ((0, 0x1008FF12), spawn "amixer set Master toggle")
+    [ ((0,                      xK_Meta_R),  nextWS)
+    , ((mod4Mask,               xK_Down),    nextWS)
+    , ((mod4Mask,               xK_Up),      prevWS)
+    , ((mod4Mask,               xK_Right),   shiftToNext)
+    , ((mod4Mask,               xK_Left),    shiftToPrev)
+    , ((mod4Mask .|. shiftMask, xK_Down),    nextScreen)
+    , ((mod4Mask .|. shiftMask, xK_Up),      prevScreen)
+    , ((mod4Mask .|. shiftMask, xK_Right),   shiftNextScreen)
+    , ((mod4Mask .|. shiftMask, xK_Left),    shiftPrevScreen)
+    , ((mod4Mask,               xK_Tab),     toggleWS)
+    , ((mod4Mask .|. shiftMask, xK_l),       spawn "xscreensaver-command -lock")
+    , ((mod4Mask .|. shiftMask, xK_p),       spawn "scrot -e 'mv $f ~/Pictures/'")
+    , ((0,                      0xFF61),     spawn "scrot -e 'mv $f ~/Pictures/'")
+    , ((0,                      0x1008FF11), spawn "amixer -q sset Master 2%-")
+    , ((0,                      0x1008FF13), spawn "amixer -q sset Master 2%+")
+    , ((0,                      0x1008FF12), spawn "amixer set Master toggle")
     ]
