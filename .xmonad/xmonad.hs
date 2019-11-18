c6b7fa892d71ba03bfffd9e6f893cc52ef7f3c07
Mort Yao <soi@mort.ninja>
2019-11-18 16:34:17 +0100

    [xmonad] update keybindings (to match GNOME's default)

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -18,20 +18,24 @@ myConfig = def
     , startupHook = spawn "dzheader" >> spawn "dzquotes"
     }
     `additionalKeys`
-    [ ((0,                      xK_Super_R), nextWS)
+    [ ((0,                      xK_Super_R), nextWS)  -- switch to next workspace
+    , ((mod4Mask,               xK_Next),    nextWS)
     , ((mod4Mask,               xK_Down),    nextWS)
+    , ((mod4Mask,               xK_Prior),   prevWS)  -- switch to previous workspace
     , ((mod4Mask,               xK_Up),      prevWS)
-    , ((mod4Mask,               xK_Right),   shiftToNext)
-    , ((mod4Mask,               xK_Left),    shiftToPrev)
-    , ((mod4Mask .|. shiftMask, xK_Down),    nextScreen)
-    , ((mod4Mask .|. shiftMask, xK_Up),      prevScreen)
-    , ((mod4Mask .|. shiftMask, xK_Right),   shiftNextScreen)
-    , ((mod4Mask .|. shiftMask, xK_Left),    shiftPrevScreen)
-    , ((mod4Mask,               xK_Tab),     toggleWS)
-    , ((mod4Mask .|. shiftMask, xK_l),       spawn "xscreensaver-command -lock")
-    , ((mod4Mask .|. shiftMask, xK_p),       spawn "scrot -e 'mv $f ~/Pictures/'")
+    , ((mod4Mask .|. shiftMask, xK_Next),    shiftToNext)  -- move window to next workspace
+    , ((mod4Mask .|. shiftMask, xK_Down),    shiftToNext)
+    , ((mod4Mask .|. shiftMask, xK_Prior),   shiftToPrev)  -- move window to previous workspace
+    , ((mod4Mask .|. shiftMask, xK_Up),      shiftToPrev)
+    , ((mod4Mask,               xK_Tab),     toggleWS)  -- toggle workspaces
+    , ((mod4Mask,               xK_Right),   nextScreen)  -- switch to next screen
+    , ((mod4Mask,               xK_Left),    prevScreen)  -- switch to previous screen
+    , ((mod4Mask .|. shiftMask, xK_Right),   shiftNextScreen)  -- move window to next screen
+    , ((mod4Mask .|. shiftMask, xK_Left),    shiftPrevScreen)  -- move window to previous screen
+    , ((mod4Mask .|. shiftMask, xK_l),       spawn "xscreensaver-command -lock")  -- lock screen
+    , ((mod4Mask .|. shiftMask, xK_p),       spawn "scrot -e 'mv $f ~/Pictures/'")  -- take screenshot
     , ((0,                      0xFF61),     spawn "scrot -e 'mv $f ~/Pictures/'")
-    , ((0,                      0x1008FF11), spawn "amixer -q sset Master 2%-")
-    , ((0,                      0x1008FF13), spawn "amixer -q sset Master 2%+")
-    , ((0,                      0x1008FF12), spawn "amixer set Master toggle")
+    , ((0,                      0x1008FF11), spawn "amixer -q sset Master 2%-")  -- volume down
+    , ((0,                      0x1008FF13), spawn "amixer -q sset Master 2%+")  -- volume up
+    , ((0,                      0x1008FF12), spawn "amixer set Master toggle")  -- mute
     ]
